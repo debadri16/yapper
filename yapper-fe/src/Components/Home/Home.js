402b4629d69af96158f90afcbf58c98df64c533b
unknown <debadri16@gmail.com>
2022-06-01 17:57:27 +0530

    Updated button and text field

--- yapper-fe/src/Components/Home/Home.js
+++ yapper-fe/src/Components/Home/Home.js
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import TextField from '@mui/material/TextField';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
 
 import './Home.css';
+import UpdateAvatarDialog from "../Dialogs/UpdateAvatarDialog";
+import { avatarsList } from "../../Common/AvatarsList";
+import defaultAvatar from "../../assets/avatars/01.png";
 
 function Home() {
 
     const [name, setName] = useState("");
     const [error, setError] = useState(false);
+    const [editAvatar, setEditAvatar] = useState(false);
+
+    // persisting index of avatar from dialog
+    const [selectedAvatarIndex, setAvatarIndex] = useState(0);
+
 
     let handleNameChange = (event) => {
         setName(event.target.value);
@@ -34,11 +46,37 @@ function Home() {
 
     return (
         <div className="bgDiv">
+            {editAvatar &&
+                <UpdateAvatarDialog handleClose={() => setEditAvatar(false)} selectedAvatarIndex={selectedAvatarIndex}
+                    handleAvatarChange={setAvatarIndex} />
+            }
             <div className="loginContainer">
-                <div className='login-detils'>
+                <div className="imgDiv" onClick={() => setEditAvatar(true)}>
+                    <img className="homeAvatar"
+                        // avatarlist imports are async, using default image if they are not loaded
+                        // xx.png -> string length is 6
+                        src={avatarsList[selectedAvatarIndex].length > 6 ? avatarsList[selectedAvatarIndex] : defaultAvatar} alt="avatar" />
+                    <ModeEditOutlineOutlinedIcon className="avatarEditBtn" />
+                </div>
+                <div className="userNameContainer">
+                    <TextField error={false} required id="standard-basic" label="Display Name" variant="standard" />
+                </div>
+                <div className='homeBtnDiv'>
+                    <button className='homeBtn' onClick={create_room}>Create Room
+                        <AddCircleOutlineOutlinedIcon className="btnIcon"/>
+                    </button>
+                </div>
+                <div className='homeBtnDiv'>
+                    <button className='homeBtn' onClick={join_room}>
+                        <span>Join Room</span>
+                        <GroupAddOutlinedIcon className="btnIcon"/>
+                    </button>
+                </div>
+                
+                
+                {/* <div className='login-detils'>
                     <div className='login-details-grp'>
                         <div>
-                            <button className='img-button'><img className='avatar' src='https://cdn0.iconfinder.com/data/icons/avatar-78/128/12-512.png' alt='mango-avatar'></img></button>
                             <div>
                                 <div className='input-data'>
                                     <input type="text" onChange={handleNameChange} required />
@@ -55,7 +93,7 @@ function Home() {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
